f46c511695063c9f8d7f691ab8e0474556870904
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-30 18:27:44 +0300

    add hometask 64 Get Natural Row In Range [N, 0]

---
+++ ex_64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке 
+// от N до 1. Выполнить с помощью рекурсии.
+
+using System;
+Console.Clear();
+
+Console.Write("Input integer number N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(GetNaturalRow(N));
+
+string GetNaturalRow (int N)
+{
+      if (N == 1)
+            return "1";
+      return N + ", " + GetNaturalRow(N - 1);
+}
